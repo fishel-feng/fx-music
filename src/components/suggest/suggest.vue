28c1a053b13ab17510b01155f5219a6a150703ad
fx109138 <1091381572@qq.com>
2017-11-11 18:35:56 +0800

    search finish

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="suggest" class="suggest">
+  <scroll ref="suggest" class="suggest" :data="result" :pullup="pullup" @scrollToEnd="searchMore">
     <ul class="suggest-list">
       <li class="suggest-item" v-for="(item,index) in result" :key="index">
         <div class="icon">
@@ -9,23 +9,22 @@
           <p class="text" v-html="getDisplayName(item)"></p>
         </div>
       </li>
-      <!-- <loading v-show="hasMore" title=""></loading> -->
+      <loading v-show="hasMore" title=""></loading>
     </ul>
-  </div>
+  </scroll>
 </template>
 
 <script type="text/ecmascript-6">
-  // import Scroll from 'base/scroll/scroll'
-  // import Loading from 'base/loading/loading'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
   // import NoResult from 'base/no-result/no-result'
   import {search} from 'api/search'
   import {ERR_OK} from 'api/config'
-  import {filterSinger} from 'common/js/song'
-  // import {createSong} from 'common/js/song'
+  import {createSong} from 'common/js/song'
   // import {mapMutations, mapActions} from 'vuex'
   // import Singer from 'common/js/singer'
   const TYPE_SINGER = 'singer'
-  // const perpage = 20
+  const PERPAGE = 20
   export default {
     props: {
       showSinger: {
@@ -40,14 +39,32 @@
     data () {
       return {
         page: 1,
-        result: []
+        result: [],
+        pullup: true,
+        hasMore: true
       }
     },
     methods: {
       search() {
-        search(this.query, this.page, this.showSinger).then(res => {
+        this.page = 1
+        this.$refs.suggest.scrollTo(0, 0)
+        this.hasMore = true
+        search(this.query, this.page, this.showSinger, PERPAGE).then(res => {
           if (res.code === ERR_OK) {
             this.result = this._genReslt(res.data)
+            this._checkMore(res.data)
+          }
+        })
+      },
+      searchMore() {
+        if (!this.hasMore) {
+          return
+        }
+        this.page++
+        search(this.query, this.page, this.showSinger, PERPAGE).then(res => {
+          if (res.code === ERR_OK) {
+            this.result = this.result.concat(this._genReslt(res.data))
+            this._checkMore(res.data)
           }
         })
       },
@@ -62,7 +79,13 @@
         if (item.type === TYPE_SINGER) {
           return item.singername
         } else {
-          return `${item.songname}-${filterSinger(item.singer)}`
+          return `${item.name}-${item.singer}`
+        }
+      },
+      _checkMore(data) {
+        const song = data.song
+        if (!song.list.length || (song.curnum + song.curpage * PERPAGE) >= song.totalnum) {
+          this.hasMore = false
         }
       },
       _genReslt(data) {
@@ -71,8 +94,17 @@
           ret.push({...data.zhida, ...{type: TYPE_SINGER}})
         }
         if (data.song) {
-          ret = ret.concat(data.song.list)
+          ret = ret.concat(this._normalizeSongs(data.song.list))
         }
+        return ret
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
         return ret
       }
     },
@@ -80,6 +112,10 @@
       query() {
         this.search()
       }
+    },
+    components: {
+      Scroll,
+      Loading
     }
   }
 </script>
